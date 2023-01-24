ec6e91d86c65207c5494135812e46c4f0a7cd79a
Deepak Singh <singhdeepak5219@gmail.com>
2023-01-24 21:15:19 +0530

    stripe payment route

--- server/Routes/appRoutes.js
+++ server/Routes/appRoutes.js
@@ -1,4 +1,9 @@
 import express from 'express';
 const router = express.Router();
+import {home,payment} from '../controllers/controllers.js';
+
+
+router.get('/',home);
+router.post('/payment',payment);
 
 export default router;
--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,5 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+import Stripe from 'stripe';
+
+const {STRIPE_SECRET_KEY} = process.env;
+const stripe = new Stripe(STRIPE_SECRET_KEY);
+
 export const home = (req,res) => {
     res.status(200).json({
         "message":"success"
     })
+}
+
+export const payment = (req,res) => {
+
+    const {product, token} = req.body;
+    console.log(`Product: ${product}`);
+    console.log(`Price: ${product.price}`);
+
+    const idempotencyKey = uuidV4();
+    return stripe.customers.create({
+        email:token.email,
+        source: token.id
+    })
+    .then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency:'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description:`Purchase of ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address:{
+                    country:token.card.address_country
+                }
+            }
+
+        },{idempotencyKey})
+        .then(result => res.status(200),json(result))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
 }
